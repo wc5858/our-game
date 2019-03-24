c5143a16480c2c0d86b9007062320c8c4479a993
Chibiao <wc47501@gmail.com>
2019-03-24 20:48:38 +0800

    职业、种族更新：图标、属性

--- src/data/careers.ts
+++ src/data/careers.ts
@@ -1,47 +1,50 @@
-import avatar1 from '../assets/avatar1.jpg'
+import avatar2 from '../assets/avatar2.jpg'
+import avatar3 from '../assets/avatar3.png'
+import avatar4 from '../assets/avatar4.png'
+import avatar5 from '../assets/avatar5.png'
 import { Career } from '../game/types';
 const data: {
     [key: string]: Career
 } = {
     '001': {
         name: '程序员',
-        avatar: avatar1,
+        avatar: avatar2,
         skillTree: {
 
         },
-        attackGrow: 1.5,
-        hpGrow: 10,
+        attackGrow: 3,
+        hpGrow: 5,
         mpGrow: 10,
     },
     '002': {
         name: '段子手',
-        avatar: avatar1,
+        avatar: avatar3,
         skillTree: {
 
         },
-        attackGrow: 1.5,
-        hpGrow: 10,
-        mpGrow: 10,
+        attackGrow: 2,
+        hpGrow: 20,
+        mpGrow: 20,
     },
     '003': {
         name: '微商',
-        avatar: avatar1,
+        avatar: avatar4,
         skillTree: {
 
         },
-        attackGrow: 1.5,
-        hpGrow: 10,
+        attackGrow: 1,
+        hpGrow: 30,
         mpGrow: 10,
     },
     '004': {
         name: '微博大V',
-        avatar: avatar1,
+        avatar: avatar5,
         skillTree: {
 
         },
-        attackGrow: 1.5,
+        attackGrow: 2,
         hpGrow: 10,
-        mpGrow: 10,
+        mpGrow: 30,
     }
 }
 export default data
--- src/data/races.ts
+++ src/data/races.ts
@@ -1,4 +1,7 @@
 import avatar1 from '../assets/avatar1.jpg'
+import avatar6 from '../assets/avatar6.png'
+import avatar3 from '../assets/avatar3.jpg'
+import avatar4 from '../assets/avatar4.jpg'
 import { Race } from '../game/types';
 const data: {
     [key: string]: Race
@@ -6,30 +9,30 @@ const data: {
     '001': {
         name: '熊猫人',
         avatar: avatar1,
-        attackGrow: 1,
-        hpGrow: 10,
-        mpGrow: 10,
+        attackGrow: 2,
+        hpGrow: 5,
+        mpGrow: 15,
     },
     '002': {
         name: '巫妖',
-        avatar: avatar1,
-        attackGrow: 1,
-        hpGrow: 10,
-        mpGrow: 10,
+        avatar: avatar3,
+        attackGrow: 3,
+        hpGrow: 5,
+        mpGrow: 5,
     },
     '003': {
         name: '鶸',
-        avatar: avatar1,
+        avatar: avatar4,
         attackGrow: 1,
-        hpGrow: 10,
-        mpGrow: 10,
+        hpGrow: 1,
+        mpGrow: 1,
     },
     '004': {
         name: '鸽子',
-        avatar: avatar1,
-        attackGrow: 1,
-        hpGrow: 10,
-        mpGrow: 10,
+        avatar: avatar6,
+        attackGrow: 2,
+        hpGrow: 2,
+        mpGrow: 20,
     }
 }
 export default data
